c937b29e6acd0e50e0560187d098d3e002bd6f46
be834194 <type.x.supernova.555@gmail.com>
2022-02-02 22:14:38 +0900

    first commit

---
+++ src/main/java/com/dining/boyaki/BoyakiDiningApplication.java
@@ -0,0 +1,13 @@
+package com.dining.boyaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoyakiDiningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoyakiDiningApplication.class, args);
+	}
+
+}
