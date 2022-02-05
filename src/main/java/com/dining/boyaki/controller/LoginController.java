b3073ceaaf6f1ae2ce95762c03d6f5a2b21ab9ed
be834194 <type.x.supernova.555@gmail.com>
2022-02-05 18:10:49 +0900

    Controllerのアノテーションを修正 #1

--- src/main/java/com/dining/boyaki/controller/LoginController.java
+++ src/main/java/com/dining/boyaki/controller/LoginController.java
@@ -2,12 +2,12 @@ package com.dining.boyaki.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/login")
+	@GetMapping("/login")
 	public String showloginPage(Model model) {
 		return "Login/Login";
 	}
--- src/main/java/com/dining/boyaki/controller/UserCalendarController.java
+++ src/main/java/com/dining/boyaki/controller/UserCalendarController.java
@@ -3,12 +3,12 @@ package com.dining.boyaki.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserCalendarController {
 	
-	@RequestMapping("/index")
+	@GetMapping("/index")
 	@PreAuthorize("hasRole('ROLE_USER')")
 	public String showUserCalendar(Model model) {
 		return "UserCalendar/UserCalendar";
