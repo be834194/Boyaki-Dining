934912f34b76325f6f1daca76d95e95b8cf4c891
be834194 <type.x.supernova.555@gmail.com>
2022-04-05 22:24:50 +0900

    UserCalendarフォルダのhtmlを修正 #39

--- src/main/java/com/dining/boyaki/config/WebMvcConfig.java
+++ src/main/java/com/dining/boyaki/config/WebMvcConfig.java
@@ -2,10 +2,8 @@ package com.dining.boyaki.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@EnableWebMvc
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	
